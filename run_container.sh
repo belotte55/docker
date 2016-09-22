052501ff5a486afeaf107ce104f46728fe6cecce
Frank BELLOTTO <fbellott@e1r12p2.42.fr>
2016-09-22 12:51:36 +0200

    22/09/2016 - 12:51

--- run_container.sh
+++ run_container.sh
@@ -6,14 +6,24 @@
 #    By: fbellott <fbellott@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/09/20 11:49:30 by fbellott          #+#    #+#              #
-#    Updated: 2016/09/21 12:25:38 by fbellott         ###   ########.fr        #
+#    Updated: 2016/09/22 12:45:19 by fbellott         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 dir=/Users/Shared
 
-if [ ! -d $dir/Web ]; then
-	cp -r Web $dir
-	chmod 700 $dir
+if [ $# -le 0 ]; then
+	echo 'Usage: '$0' <image_name> <dir_to_copy>'
+	exit 1
 fi
-docker run -P -it -v /Users/Shared/Web:/root/Web $1
+
+if [ $# -ge 1 ] && [ ! -d $dir/Web ]; then
+	cp -r $2 $dir/Server &&
+		chmod 700 $dir/Server &&
+		echo $2' was copied to '$dir'/Server. Remeber to delete it ! ;]' &&
+		docker run -P -it -v /Users/Shared/Server:/root/Server $1 /bin/bash
+else
+	docker run -P -it $1 /bin/bash
+fi
+
+exit 0
