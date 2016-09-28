6c33fd16e297846c50d37b6c8c6bee75324df33b
Frank BELLOTTO <fbellott@e3r6p15.42.fr>
2016-09-28 14:15:55 +0200

    28/09/2016 - 14:15

--- install_machine.sh
+++ install_machine.sh
@@ -6,20 +6,25 @@
 #    By: fbellott <fbellott@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/09/19 13:36:38 by fbellott          #+#    #+#              #
-#    Updated: 2016/09/21 12:32:40 by fbellott         ###   ########.fr        #
+#    Updated: 2016/09/28 14:13:04 by fbellott         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 MACHINE_NAME='default'
 
+if [ -d ~/.docker ]; then
+	echo 'You have a ~/.docker directory. Please rm/mv it.'
+	exit 1
+fi
+
 echo "export MACHINE_STORAGE_PATH=/sgoinfre/goinfre/Perso/`whoami`" >> ~/.zshrc
 chmod +x ~/.zshrc
-~/.zshrc
+source ~/.zshrc
 chmod 700 /sgoinfre/goinfre/Perso/`whoami`
 docker-machine create --driver virtualbox $MACHINE_NAME
-#cp -r ~/.docker/* /sgoinfre/goinfre/Perso/`whoami`/docker/
-#mv ~/.docker ~/.Trash/docker
-#ln -s /sgoinfre/goinfre/Perso/`whoami`/docker ~/.docker
 docker-machine env $MACHINE_NAME >> ~/.zshrc
 echo 'eval "$(docker-machine env '$MACHINE_NAME')"' >> ~/.zshrc
-~/.zshrc
+source ~/.zshrc
+docker-machine stop $MACHINE_NAME
+vboxmanage modifyvm default --natpf1 "nameformapping,tcp,,1337,,1337"
+docker-machine start $MACHINE_NAME
--- run_container.sh
+++ run_container.sh
@@ -6,24 +6,29 @@
 #    By: fbellott <fbellott@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/09/20 11:49:30 by fbellott          #+#    #+#              #
-#    Updated: 2016/09/22 12:45:19 by fbellott         ###   ########.fr        #
+#    Updated: 2016/09/26 15:17:17 by fbellott         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 dir=/Users/Shared
+container_name='sails'
+#container_name=$1
 
-if [ $# -le 0 ]; then
-	echo 'Usage: '$0' <image_name> <dir_to_copy>'
-	exit 1
-fi
+#if [ $# -le 0 ]; then
+#	echo 'Usage: '$0' <image_name> <dir_to_copy>'
+#	exit 1
+#fi
 
-if [ $# -ge 1 ] && [ ! -d $dir/Web ]; then
+if [ $# -ge 2 ] && [ ! -d $dir/Server ]; then
 	cp -r $2 $dir/Server &&
 		chmod 700 $dir/Server &&
-		echo $2' was copied to '$dir'/Server. Remeber to delete it ! ;]' &&
-		docker run -P -it -v /Users/Shared/Server:/root/Server $1 /bin/bash
+		echo $2' was copied to '$dir'/Server. Remeber to delete it ! ;]'
+fi
+
+if [ "`docker ps | grep '1337'`" == '' ]; then
+	docker run -p 1337:1337 -it --add-host="localhost:192.168.99.100" -v /Users/Shared/Server:/root/Server $container_name /bin/bash
 else
-	docker run -P -it $1 /bin/bash
+	docker run -it --add-host="localhost:192.168.99.100" -v /Users/Shared/Server:/root/Server $container_name /bin/bash
 fi
 
 exit 0
